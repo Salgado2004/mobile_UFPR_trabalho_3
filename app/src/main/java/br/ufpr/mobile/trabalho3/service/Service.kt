a2480a7e4962ebe4dd3ba5adf82a51b5f3c1ba7c
Mateus Bazan <mateus.bazan@gmail.com>
2025-06-09 21:30:33 -0300

    continuation

--- app/src/main/java/br/ufpr/mobile/trabalho3/service/Service.kt
+++ app/src/main/java/br/ufpr/mobile/trabalho3/service/Service.kt
@@ -3,6 +3,8 @@ package br.ufpr.mobile.trabalho3.service
 import br.ufpr.mobile.trabalho3.model.AvailableCurrencies
 import br.ufpr.mobile.trabalho3.model.CurrencyAPI
 import br.ufpr.mobile.trabalho3.model.Envelope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -22,7 +24,11 @@ class Service {
 
     suspend fun getConversao(moedas: AvailableCurrencies, valor: Double): Double{
         try{
-            envelope = currencyAPI.getQuotation(moedas.sigla)
+
+            envelope = withContext(Dispatchers.IO){
+                currencyAPI.getQuotation(moedas.sigla)
+            }
+
             when (moedas) {
                 AvailableCurrencies.USDBRL -> return (valor * (envelope.usdbrl.high + envelope.usdbrl.low)/2)
                 AvailableCurrencies.BRLUSD -> return (valor * (envelope.brlusd.high + envelope.brlusd.low)/2)
@@ -32,8 +38,9 @@ class Service {
                 AvailableCurrencies.BRLBTC -> return (valor / (envelope.btcbrl.high + envelope.btcbrl.low)/2)
                 else -> return valor
             }
+
         } catch (e: Exception) {
-            println("Erro: +${e.message}")
+            println("Erro: ${e.message}")
             return 0.0
         }
     }
