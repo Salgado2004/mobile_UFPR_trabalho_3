6857351294de082e28f71053cc24a3942eea47b4
Mateus Bazan <mateus.bazan@gmail.com>
2025-06-09 21:16:52 -0300

    service & class fix

--- app/src/main/java/br/ufpr/mobile/trabalho3/model/AvailableCurrencies.kt
+++ app/src/main/java/br/ufpr/mobile/trabalho3/model/AvailableCurrencies.kt
@@ -1,10 +1,10 @@
 package br.ufpr.mobile.trabalho3.model
 
-enum class AvailableCurrencies(sigla: String, nome: String) {
-    USDBRL("USD-BRL", "Dólar Americano/Real Brasileiro"),
-    BRLUSD("BRL-USD", "Real Brasileiro/Dólar Americano"),
-    BTCUSD("BTC-USD", "Bitcoin/Dólar Americano"),
-    BTCBRL("BTC-BRL", "Bitcoin/Real Brasileiro"),
-    USDBTC("BTC-USD", "Dólar Americano/Bitcoin"),
-    BRLBTC("BTC-BRL", "Real Brasileiro/Bitcoin")
+enum class AvailableCurrencies(val sigla: String) {
+    USDBRL("USD-BRL"),
+    BRLUSD("BRL-USD"),
+    BTCUSD("BTC-USD"),
+    BTCBRL("BTC-BRL"),
+    USDBTC("BTC-USD"),
+    BRLBTC("BTC-BRL")
 }
---
+++ app/src/main/java/br/ufpr/mobile/trabalho3/service/Service.kt
@@ -0,0 +1,40 @@
+package br.ufpr.mobile.trabalho3.service
+
+import br.ufpr.mobile.trabalho3.model.AvailableCurrencies
+import br.ufpr.mobile.trabalho3.model.CurrencyAPI
+import br.ufpr.mobile.trabalho3.model.Envelope
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class Service {
+
+    private lateinit var currencyAPI: CurrencyAPI
+    private lateinit var envelope: Envelope
+
+    val retrofit = Retrofit.Builder()
+        .baseUrl("https://economia.awesomeapi.com.br")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    init {
+        currencyAPI = retrofit.create(CurrencyAPI::class.java)
+    }
+
+    suspend fun getConversao(moedas: AvailableCurrencies, valor: Double): Double{
+        try{
+            envelope = currencyAPI.getQuotation(moedas.sigla)
+            when (moedas) {
+                AvailableCurrencies.USDBRL -> return (valor * (envelope.usdbrl.high + envelope.usdbrl.low)/2)
+                AvailableCurrencies.BRLUSD -> return (valor * (envelope.brlusd.high + envelope.brlusd.low)/2)
+                AvailableCurrencies.BTCUSD -> return (valor * (envelope.btcusd.high + envelope.btcusd.low)/2)
+                AvailableCurrencies.BTCBRL -> return (valor * (envelope.btcbrl.high + envelope.btcbrl.low)/2)
+                AvailableCurrencies.USDBTC -> return (valor / (envelope.btcusd.high + envelope.btcusd.low)/2)
+                AvailableCurrencies.BRLBTC -> return (valor / (envelope.btcbrl.high + envelope.btcbrl.low)/2)
+                else -> return valor
+            }
+        } catch (e: Exception) {
+            println("Erro: +${e.message}")
+            return 0.0
+        }
+    }
+}
